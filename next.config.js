f2674e3dfa1a1f608348769817339a476bd54a45
hannemanrichard <104073700+hannemanrichard@users.noreply.github.com>
2024-01-20 16:35:18 +0100

    change origin

--- next.config.js
+++ next.config.js
@@ -9,20 +9,9 @@ const nextConfig = {
       'a0.muscache.com'
     ],
   },
-  async headers() {
-  return [
-      {
-          // matching all API routes
-          source: "/api/:path*",
-          headers: [
-              { key: "Access-Control-Allow-Credentials", value: "true" },
-              { key: "Access-Control-Allow-Origin", value: "https://jomayang.github.io/capsule-endo/" }, // replace this your actual origin
-              { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-              { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-          ]
-      }
-  ]
-},
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+  },
 }
 
 module.exports = nextConfig
